0ff2e9a7e93d9529bb4522785ffde376eb0d7cbd
MaxwellGordonC <89608188+MaxwellGordonC@users.noreply.github.com>
2022-08-31 22:08:04 -0500

    31:30

---
+++ Prototype1Runthrough/Assets/Scripts/CamFollowPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowPlayer : MonoBehaviour
+{
+    // MaxG: Get Player ref.
+    public GameObject player;
+    private Vector3 cam_offset = new Vector3(0, 5, -10);
+
+    void Update()
+    {
+        transform.position = player.transform.position + cam_offset;
+    }
+}
---
+++ Prototype1Runthrough/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+    public float forwardInput;
+    public float horizontalInput;
+
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // MaxG: Move forward with forward input.
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
