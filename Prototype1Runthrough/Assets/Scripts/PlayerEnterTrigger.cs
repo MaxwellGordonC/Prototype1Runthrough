5c9ca22b1f10cc9cb8e3c79fbed7101e18c619a6
MaxwellGordonC <89608188+MaxwellGordonC@users.noreply.github.com>
2022-09-02 16:23:39 -0500

    Win and lose

---
+++ Prototype1Runthrough/Assets/Scripts/PlayerEnterTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerEnterTrigger : MonoBehaviour
+{
+    public Text textbox;
+    private void OnTriggerEnter(Collider other)
+    {
+        if ( other.CompareTag("TriggerEndGame") )
+        {
+            textbox.text = "You win!";
+        }
+    }
+}
